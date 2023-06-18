82ee69141928c9fa352f452880d631fe2b282da8
bishal <bishalpd@gmail.com>
2023-06-18 07:16:55 +0600

    first commit

---
+++ src/app/about/page.js
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: "About page metadata",
+  description: "lalalala",
+};
+
+const Index = () => {
+  return <>This is about us page</>;
+};
+
+export default Index;
